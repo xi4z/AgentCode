3784257c5efecc14ea25a7654ce876744a087af3
Reyeraz <reyeraz@users.noreply.github.com>
2026-08-23 18:53:43 +0800

    Revert "refactor: 用 AgentSessionRegistry 管理会话，让 AgentSession 支持 Spring Bean 注入"
    This reverts commit 00a5a4dee5fccfbdb5cfa60e96b76ed5aa78844c.

--- src/main/java/com/agentcode/AgentCodeApplication.java
+++ src/main/java/com/agentcode/AgentCodeApplication.java
@@ -3,11 +3,9 @@ package com.agentcode;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
-@EnableScheduling
 public class AgentCodeApplication {
     public static void main(String[] args) {
         SpringApplication.run(AgentCodeApplication.class, args);
